aa289e81dbc6e774747e5d6c3217a68ad2f02eff
davetallon <davetallon@gmail.com>
2020-11-04 10:59:05 +0000

    Initial commit

---
+++ Watch/script_main.js
@@ -0,0 +1,100 @@
+const thumbPlaystation = document.querySelector('.playstation');
+const thumb3M = document.querySelector('.film3M');
+const thumbPropertyFinder = document.querySelector('.propertyFinder');
+const thumbSungod = document.querySelector('.sungod');
+const thumbJuliaBiel = document.querySelector('.juliaBiel');
+const thumbCesar = document.querySelector('.cesar');
+
+const filmPlaystation = document.querySelector('.playstationLink');
+const film3M = document.querySelector('.link3M');
+const filmPropertyFinder = document.querySelector('.propertyFinderLink');
+const filmSungod = document.querySelector('.sungodLink');
+const filmJuliaBiel = document.querySelector('.juliaBielLink');
+const filmCesar = document.querySelector('.cesarLink');
+
+const modal = document.querySelector('.modalBackdrop');
+const videoWindow = document.querySelector('videoWindow');
+const thumbnails = document.querySelectorAll('.thumbnail');
+
+
+let player = new Vimeo.Player('vimeoPS');
+
+
+loadEventListeners();
+
+
+
+function loadEventListeners() {
+    thumbPlaystation.addEventListener('click', function playPS() {
+        filmPlaystation.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeoPS');
+    });
+    thumb3M.addEventListener('click', function play3M() {
+        film3M.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeo3M');
+    });
+    thumbPropertyFinder.addEventListener('click', function playPF() {
+        filmPropertyFinder.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeoPF');
+    });
+    thumbSungod.addEventListener('click', function playSG() {
+        filmSungod.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeoSG');
+    });
+    thumbJuliaBiel.addEventListener('click', function playJB() {
+        filmJuliaBiel.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeoJB');
+    });
+    thumbCesar.addEventListener('click', function playCes() {
+        filmCesar.style.display = 'inline-block';
+        player = new Vimeo.Player('vimeoCE');
+        // player.on('play', function() {
+        //     console.log('played the video!');
+        //     });
+        //     player.on('pause', function() {
+        //     console.log('paused the video!');   
+        //     });
+        //     player.getVideoTitle().then(function(title) {
+        //     console.log('title:', title);
+        //     });
+    });
+}
+
+
+for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+}
+
+
+if (modal) {
+    modal.addEventListener('click', function() {
+        modal.style.display = 'none';
+        player.pause();
+        player.unload();
+        reset();
+    });
+}
+
+
+function reset() {
+    if (filmPlaystation) {
+        filmPlaystation.style.display = 'none';
+    }
+    if (film3M) {
+        film3M.style.display = 'none';
+    }
+    if (filmPropertyFinder) {
+        filmPropertyFinder.style.display = 'none';
+    }
+    if (filmSungod) {
+        filmSungod.style.display = 'none';
+    }
+    if (filmJuliaBiel) {
+        filmJuliaBiel.style.display = 'none';
+    }
+    if (filmCesar) {
+        filmCesar.style.display = 'none';
+    }
+}
